ed1ba726d0570519b1ab8c460eece3181aa4fa20
Perry <petreli1@ymail.com>
2022-06-21 11:38:10 +0100

    added background video that scales with screen resolution

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import video from './video/Footage_Final.mp4';
+import background from './images/Final_Render.png';
+
+import videoStyle from './style/video.module.css';
 
 function App() {
   return (
     <div className="App">
+      <div className={videoStyle.container}>
+        <video className={videoStyle.background} autoPlay loop muted>
+          <source src={video} type='video/mp4' />
+        </video>
+      </div>
       
     </div>
   );
