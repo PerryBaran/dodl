9f211156f121c95c67d570eb0a2a201634829518
Perry <petreli1@ymail.com>
2022-07-05 17:20:15 +0100

    updated MediaControls to hide/show className the same way as every other element

--- src/components/main/media-controls/MediaControls.js
+++ src/components/main/media-controls/MediaControls.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import style from './mediaControls.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
+import AppContext from '../../../utils/context/AppContext';
 
 const MediaControls = (props) => {
     const { isPlaying, setIsPlaying, skipSong } = props
-
-    const hideWhilePlaying = () => {
-        return isPlaying ? style.hideButton : style.showButton;
-    };
+    const { hideWhilePlaying } = useContext(AppContext)
 
     return (
         <div className={`centerFlex positionTop`}>
                 <div className={`${style.controls} ${style.middle}`}>
-                    <button className={`${style.backwards} ${hideWhilePlaying()}`} onClick={() => skipSong(false)}>
+                    <button className={`${style.control} ${style.backwards} ${hideWhilePlaying(style.pause)}`} onClick={() => skipSong(false)}>
                         <FontAwesomeIcon icon={faBackward} />
                     </button>
-                    <button className={`${style.playPause} ${hideWhilePlaying()}`} onClick={() => setIsPlaying(!isPlaying)}>
+                    <button className={`${style.control} ${style.playPause} ${hideWhilePlaying(style.pause)}`} onClick={() => setIsPlaying(!isPlaying)}>
                         <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
                     </button>
-                    <button className={`${style.forward} ${hideWhilePlaying()}`}>
+                    <button className={`${style.control} ${style.forward} ${hideWhilePlaying(style.pause)}`}>
                         <FontAwesomeIcon icon={faForward} onClick={() => skipSong()}/>
                     </button>
                 </div>
